dc544d014e3cbbc22eeef6a4ad8de88597224799
Aden1ke <sarahogunlalu@.com>
2025-04-14 08:19:29 +0100

    portfolio skeleton

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './app.scss'
+const App = () => {
+	return <div>
+		<section>Hero</section>
+		<section>parallex</section>
+		<section>service</section>
+		<section>parallex</section>
+		<section>Portfolio</section>
+		<section>portfolio</section>
+		<section>Portfolio</section>
+		<section>Contact</section>
+	</div>;
+};
+
+export default App;
