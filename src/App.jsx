68eca80a7a30c96638c39c649baa92817f556863
Aden1ke <sarahogunlalu@.com>
2025-08-26 05:57:37 +0100

    responsiveness of the hero page to fit the side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
 import './app.scss'
-//import Sidebar from './components/sidebar/Sidebar';
-import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+//import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Projects from "./components/projects/Projects";
+//import About from './components/about/About';
+//import Contact from './components/contact/Contact';
+//import Projects from "./components/projects/Projects";
 
 
 
 const App = () => {
-	return <div>
-		<section id="Homepage">
-    <Navbar/>
-    {/*<Hero>*/}
-    <Home/>
-    </section>
-		<section id="About">
-    <About/>
-    </section>
-		<section id="Projects">
-    <Projects/>
-    </section>
-		<section id="Contact">
-    <Contact />
-    </section>
-	</div>;
-};
+    return (
+      <div className="main-container">
+        <Sidebar />
+        <div className="content">
+          <section id="Homepage">
+            <Home />
+          </section>
 
+        {/* Other sections would go here inside the 'content' div */}
+        {/*
+        <section id="About">
+          <About />
+        </section>
+        <section id="Projects">
+          <Projects />
+        </section>
+        <section id="Contact">
+          <Contact />
+        </section>*/}
+        </div>
+      </div>
+  );
+};
+  
 export default App;
